f00695a53eb7fa958be0d9f70f88a3edaa77d38e
jeffrycoder <jeffrycoder@gmail.com>
2023-06-27 17:56:40 -0400

    Agregando primer commit proyecto de backendJava

---
+++ primerCursoJava/src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Intro with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Hola mundo!");
+    }
+}
